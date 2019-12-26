299dd41c0b0bb63a937ed7552a37090197221f76
Stefan Rimaila <stefan@rimaila.fi>
2019-12-26 19:08:47 +0200

    stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import * as React from 'karet';
 import * as U from 'karet.util';
+import * as L from 'partial.lenses';
 import * as K from 'kefir';
+
+import './App.scss';
 
 import { watchCurrentPosition } from './core/location';
 
 const App = () => {
   const pos = watchCurrentPosition().toProperty();
+
+  const asSixDec = L.reread(n => n.toFixed(6));
 
   const err = U.thru(
     pos,
@@ -18,17 +23,14 @@ const App = () => {
 
   return (
     <main>
-      <fieldset>
-        <legend>Debug</legend>
-
-        <pre>
-          <code>{U.stringify(pos, null, 2)}</code>
-        </pre>
-
-        <pre>
-          <code>{U.stringify(err, null, 2)}</code>
-        </pre>
-      </fieldset>
+      <div className="position">
+        <div className="value">
+          {U.view(['coords', 'latitude', asSixDec], pos)}
+        </div>
+        <div className="value">
+          {U.view(['coords', 'longitude', asSixDec], pos)}
+        </div>
+      </div>
     </main>
   );
 };
